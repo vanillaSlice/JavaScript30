fe39de9d991f6d51ff87f01d99a9ef23f1a37393
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2017-07-29 12:36:58 +0100

    Added Hold Shift and Check Checkboxes

---
+++ 10 - Hold Shift and Check Checkboxes/main.js
@@ -0,0 +1,21 @@
+(() => {
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  let lastChecked;
+
+  checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
+
+  function handleCheck(e) {
+    let inBetween = false;
+    if (e.shiftKey && this.checked) {
+      checkboxes.forEach(checkbox => {
+        if (checkbox === this || checkbox === lastChecked) {
+          inBetween = !inBetween;
+        }
+        if (inBetween) {
+          checkbox.checked = true;
+        }
+      });
+    }
+    lastChecked = this;
+  }
+})();
