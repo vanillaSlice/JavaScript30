53abb2b962ebc3a349dc522048ad361428b2dd2d
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2017-07-30 16:35:12 +0100

    Added key sequence detection

---
+++ 12 - Key Sequence Detection/main.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', () => {
+
+  const pressed = [];
+  const secretCode = 'vanillaslice';
+
+  window.addEventListener('keyup', event => {
+    pressed.push(event.key);
+    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+    if (pressed.join('').includes(secretCode)) {
+      cornify_add();
+    }
+  });
+
+});
