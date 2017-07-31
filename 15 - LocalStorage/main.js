313b7084594d0cf744ec2bbd79e5726656ee8d1e
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2017-07-31 11:47:43 +0100

    Completed LocalStorage

---
+++ 15 - LocalStorage/main.js
@@ -0,0 +1,85 @@
+window.addEventListener('load', () => {
+
+  /*
+   * Elements
+   */
+
+  const clearAll = document.querySelector('[name="clear-all"]');
+  const checkAll = document.querySelector('[name="check-all"]');
+  const uncheckAll = document.querySelector('[name="uncheck-all"]');
+  const addItems = document.querySelector('.add-items');
+  const itemsList = document.querySelector('.plates');
+  const items = JSON.parse(localStorage.getItem('items')) || [];
+
+  /*
+   * Functions
+   */
+
+  function clearItems() {
+    items.length = 0;
+    localStorage.removeItem('items');
+    populateList(items, itemsList);
+  }
+
+  function checkItems() {
+    items.forEach(item => item.done = true);
+    localStorage.setItem('items', JSON.stringify(items));
+    populateList(items, itemsList);
+  }
+
+  function uncheckItems() {
+    items.forEach(item => item.done = false);
+    localStorage.setItem('items', JSON.stringify(items));
+    populateList(items, itemsList);
+  }
+
+  function addItem(e) {
+    e.preventDefault();
+    const text = this.querySelector('[name=item]').value;
+    const item = {
+      text,
+      done: false
+    };
+    items.push(item);
+    populateList(items, itemsList);
+    localStorage.setItem('items', JSON.stringify(items));
+    this.reset();
+  }
+
+  function populateList(plates = [], platesList) {
+    platesList.innerHTML = plates.map((plate, index) =>
+      `
+      <li>
+        <input type="checkbox" data-index="${index}" id="item${index}" ${plate.done ? 'checked' : ''}>
+        <label for="item${index}">${plate.text}</label>
+      </li>
+      `
+    ).join('');
+  }
+
+  function toggleDone(e) {
+    if (!e.target.matches('input')) return;
+    const el = e.target;
+    const index = el.dataset.index;
+    items[index].done = !items[index].done;
+    localStorage.setItem('items', JSON.stringify(items));
+    populateList(items, itemsList);
+  }
+
+  /*
+   * Event listeners
+   */
+
+  clearAll.addEventListener('click', clearItems);
+  checkAll.addEventListener('click', checkItems);
+  uncheckAll.addEventListener('click', uncheckItems);
+  addItems.addEventListener('submit', addItem);
+  itemsList.addEventListener('click', toggleDone);
+
+  /*
+   * Init
+   */
+
+  populateList(items, itemsList);
+
+});
