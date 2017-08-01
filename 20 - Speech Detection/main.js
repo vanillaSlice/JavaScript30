295359a5d655cc51baecc8a2511eca961ec742d5
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2017-08-01 22:23:37 +0100

    Completed Speech Detection

---
+++ 20 - Speech Detection/main.js
@@ -0,0 +1,43 @@
+window.addEventListener('load', () => {
+
+  /*
+   * Elements 
+   */
+
+  const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  const recognition = new SpeechRecognition();
+  recognition.interimResults = true;
+  const words = document.querySelector('.words');
+  let p = document.createElement('p');
+  words.appendChild(p);
+
+  /*
+   * Functions 
+   */
+
+  function handleResult(e) {
+    const transcript = Array.from(e.results)
+      .map(result => result[0])
+      .map(result => result.transcript)
+      .join('');
+    p.textContent = transcript;
+    if (e.results[0].isFinal) {
+      p = document.createElement('p');
+      words.appendChild(p);
+    }
+  }
+
+  /*
+   * Event listeners 
+   */
+
+  recognition.addEventListener('result', handleResult);
+  recognition.addEventListener('end', recognition.start);
+
+  /*
+   * Init 
+   */
+
+  recognition.start();
+
+});
