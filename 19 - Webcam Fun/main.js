0d130ceb597a804033d077f83a9283e4903513b2
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2017-08-01 19:46:33 +0100

    Added Speech Detection

--- 19 - Webcam Fun/main.js
+++ 19 - Webcam Fun/main.js
@@ -30,9 +30,9 @@ window.addEventListener('load', () => {
       ctx.drawImage(video, 0, 0, width, height);
       let pixels = ctx.getImageData(0, 0, width, height);
       //pixels = redEffect(pixels);
-      // pixels = rgbSplit(pixels);
-      // ctx.globalAlpha = 0.1;
-      pixels = greenScreen(pixels);
+      pixels = rgbSplit(pixels);
+      //ctx.globalAlpha = 0.1;
+      //pixels = greenScreen(pixels);
       ctx.putImageData(pixels, 0, 0);
     }, 16);
   }
@@ -61,7 +61,7 @@ window.addEventListener('load', () => {
 
   function rgbSplit(pixels) {
     for (let i = 0; i < pixels.data.length; i += 4) {
-      pixels.data[i - 150] = pixels.data[i];
+      pixels.data[i - 350] = pixels.data[i];
       pixels.data[i + 100] = pixels.data[i + 1];
       pixels.data[i - 250] = pixels.data[i + 2];
     }
