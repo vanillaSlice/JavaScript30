29783fb077baa85ff816dff04000c8659cf52736
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2017-08-02 13:55:23 +0100

    Completed Geolocation

--- 21 - Geolocation/main.js
+++ 21 - Geolocation/main.js
@@ -2,9 +2,13 @@ window.addEventListener('load', () => {
 
   const arrow = document.querySelector('.arrow');
   const speed = document.querySelector('.speed-value');
+  const latitude = document.querySelector('.latitude-value');
+  const longitude = document.querySelector('.longitude-value');
 
   navigator.geolocation.watchPosition(data => {
     speed.textContent = data.coords.speed || 0;
+    latitude.textContent = data.coords.latitude.toFixed(5);
+    longitude.textContent = data.coords.longitude.toFixed(5);
     arrow.style.transform = `rotate(${data.coords.heading}deg)`;
   }, err => {
     console.error(err);
